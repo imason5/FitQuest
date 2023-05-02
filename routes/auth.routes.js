0a80186d1b5d9e0f5af82ce97e89d1324a454147
Liz <claireliang2012@gmail.com>
2023-05-02 20:38:13 +0800

    Use cloudinary on signup page

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,8 @@ const bcryptjs = require("bcryptjs");
 const { validateSignupInput } = require("../middleware/inputValidation");
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
 
+const uploader = require("../middleware/cloudinary.config.js");
+
 const roundOfSalt = 13;
 const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
 
@@ -14,59 +16,78 @@ router.get("/signup", (req, res, next) => {
 });
 
 /* --- POST: signup page --- */
-router.post("/signup", validateSignupInput, async (req, res, next) => {
-  try {
-    const isExistingUsername = await User.findOne({
-      username: req.body.username,
-    });
-    const isExistingEmail = await User.findOne({
-      email: req.body.email,
-    });
+router.post(
+  "/signup",
+  /*validateSignupInput,*/
+  uploader.single("imageUrl"),
+  async (req, res, next) => {
+    try {
+      console.log("req.body: ", req.body);
+      console.log("req.file: ", req.file);
 
-    if (!isExistingUsername) {
-      if (!isExistingEmail) {
-        if (pwdRegex.test(req.body.password)) {
-          const salt = bcryptjs.genSaltSync(roundOfSalt);
-          const passwordHash = bcryptjs.hashSync(req.body.password, salt);
-          // Create a new user via User model
-          await User.create({
-            username: req.body.username,
-            password: passwordHash,
-            email: req.body.email,
-            age: req.body.age,
-            gender: req.body.gender,
-            weight: req.body.weight,
-            height: req.body.height,
-            bio: req.body.bio,
-          });
-          // After Signing up, then redirect to login page - redirect
-          res.redirect("/login");
+      const isExistingUsername = await User.findOne({
+        username: req.body.username,
+      });
+      const isExistingEmail = await User.findOne({
+        email: req.body.email,
+      });
+
+      if (!isExistingUsername) {
+        if (!isExistingEmail) {
+          if (pwdRegex.test(req.body.password)) {
+            const salt = bcryptjs.genSaltSync(roundOfSalt);
+            const passwordHash = bcryptjs.hashSync(req.body.password, salt);
+
+            //get the file url from cloudinary
+            console.log("file is: ", req.file);
+
+            if (!req.file) {
+              console.log("there was an error uploading the file");
+              next(new Error("No file uploaded!"));
+              return;
+            }
+
+            // Create a new user via User model
+            await User.create({
+              username: req.body.username,
+              password: passwordHash,
+              email: req.body.email,
+              age: req.body.age,
+              gender: req.body.gender,
+              weight: req.body.weight,
+              height: req.body.height,
+              bio: req.body.bio,
+              profilePic: req.file.path,
+            });
+            // After Signing up, then redirect to login page - redirect
+            res.redirect("/login");
+          } else {
+            // When psw is not strong enough
+            res.render("auth/signup", {
+              username: req.body.username,
+              errorMessage: "Password is not strong enough",
+            });
+          }
         } else {
-          // When psw is not strong enough
+          // When email is already taken
           res.render("auth/signup", {
             username: req.body.username,
-            errorMessage: "Password is not strong enough",
+            email: req.body.email,
+            errorMessage: "Email is in use",
           });
         }
       } else {
-        // When email is already taken
+        // When username is already existed
         res.render("auth/signup", {
           username: req.body.username,
-          email: req.body.email,
-          errorMessage: "Email is in use",
+          errorMessage: "Username is in use",
         });
       }
-    } else {
-      // When username is already existed
-      res.render("auth/signup", {
-        username: req.body.username,
-        errorMessage: "Username is in use",
-      });
+    } catch (error) {
+      console.log("Error from signup post: ", error);
     }
-  } catch (error) {
-    console.log("Error from signup post: ", error);
   }
-});
+);
 
 /* --- GET: login page --- */
 router.get("/login", (req, res, next) => {
