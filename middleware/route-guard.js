ce8e234f7aba68643d8f7a474203fa8d72a20739
Liz <claireliang2012@gmail.com>
2023-05-03 20:51:48 +0800

    Fix navbar problem with new route-guard middleware

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,15 +1,3 @@
-/*const redirectIf = (condition, redirectPath) => (req, res, next) => {
-  if (condition(req)) {
-    return res.redirect(redirectPath);
-  }
-  next();
-};
-
-const isLoggedIn = redirectIf((req) => !req.session.loggedInUser, "/login");
-
-const isLoggedOut = redirectIf((req) => req.session.loggedInUser, "/"); */
-
-// checks if the user is logged in when trying to access a specific page
 const isLoggedIn = (req, res, next) => {
   if (!req.session.loggedInUser) {
     return res.render("index", { navSwitch: false });
@@ -17,8 +5,6 @@ const isLoggedIn = (req, res, next) => {
   next();
 };
 
-// if an already logged in user tries to access the login page it
-// redirects the user to the home page
 const isLoggedOut = (req, res, next) => {
   if (req.session.loggedInUser) {
     return res.render("index", { navSwitch: false });
@@ -30,3 +16,15 @@ module.exports = {
   isLoggedIn,
   isLoggedOut,
 };
+
+/* Original middleware here just in case */
+
+/* const redirectIf = (condition, redirectPath) => (req, res, next) => {
+  if (condition(req)) {
+    return res.redirect(redirectPath);
+  }
+  next();
+};
+
+const isLoggedIn = redirectIf((req) => !req.session.loggedInUser, "/login");
+const isLoggedOut = redirectIf((req) => req.session.loggedInUser, "/"); */
